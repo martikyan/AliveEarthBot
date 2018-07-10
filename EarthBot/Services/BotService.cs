3770dfa16b85251899884e966194c684fe3db113
Karen Martikyan <kamartikyan@gmail.com>
2018-07-10 19:18:46 +0400

    Adding logging (#2)
    Logging added to the project

--- EarthBot/Services/BotService.cs
+++ EarthBot/Services/BotService.cs
@@ -3,23 +3,23 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
 using Tweetinvi;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;
 
-namespace EarthBot.Services
-{
-    public class BotService
-    {
+namespace EarthBot.Services {
+    public class BotService {
         private readonly IConfiguration _configuration;
         private readonly ImageService _imageService;
+        private readonly ILogger _logger;
         private readonly string[] _symbols;
 
-        public BotService(IConfiguration configuration)
-        {
+        public BotService(IConfiguration configuration, ILogger<BotService> logger) {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-            _imageService = new ImageService(_configuration);
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _imageService = new ImageService(_configuration, _logger);
 
             _symbols = _configuration["TweetSymbols"].Split(' ');
 
@@ -31,20 +31,25 @@ namespace EarthBot.Services
             Auth.SetUserCredentials(customerKey, customerKeySecret, accessToken, accessTokenSecret);
         }
 
-        public void Init()
-        {
-            while (true)
-            {
-                var hoursToSleep = int.Parse(_configuration["SleepTimeInHours"]);
-                var publishTask = new Task(TryPublishImage);
+        public void Init() {
+            var hoursToSleep = int.Parse(_configuration["SleepTimeInHours"]);
 
-                publishTask.Start();
+            if (hoursToSleep == 0) {
+                var e = new ArgumentNullException(nameof(hoursToSleep));
+                _logger.LogError(e.Message);
+
+                throw e;
+            }
+
+            while (true) {
+                new Task(TryPublishImage).Start();
+
+                _logger.LogInformation($"Main thread is sleeping for {hoursToSleep} hours");
                 Thread.Sleep(new TimeSpan(0, hoursToSleep, 0, 0));
             }
         }
 
-        private async void TryPublishImage()
-        {
+        private async void TryPublishImage() {
             var random = new Random();
             var stream = new MemoryStream();
             var pictureToPost = await _imageService.GetPostableImage();
@@ -58,6 +63,7 @@ namespace EarthBot.Services
                         $"{symbol}    ({coordinates.Latitude:0.######}, {coordinates.Longitude:0.######})")
                     {MediaBinaries = {stream.GetBuffer()}, Coordinates = coordinates};
 
+            _logger.LogInformation($"Tweeting {parameters.Text}");
             Tweet.PublishTweet(parameters);
         }
     }
--- EarthBot/Services/ImageService.cs
+++ EarthBot/Services/ImageService.cs
@@ -4,70 +4,66 @@ using System.Threading.Tasks;
 using BingMapsRESTToolkit;
 using EarthBot.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Filters;
 using SixLabors.ImageSharp.Processing.Transforms;
 
-namespace EarthBot.Services
-{
-    public class ImageService
-    {
+namespace EarthBot.Services {
+    public class ImageService {
         private const double LatitudeMaxModulus = 85.0d;
         private const double LongtitudeMaxModulus = 180.0d;
         private const double Detailness = 1.0d;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
 
-        public ImageService(IConfiguration configuration)
-        {
-            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        public ImageService(IConfiguration configuration, ILogger logger) {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        private IConfiguration Configuration { get; }
-
-        private LocatedObject<string> GeneratePictureUrl()
-        {
+        private LocatedObject<string> GeneratePictureUrl() {
             var rnd = new Random();
             var latitude = rnd.NextDouble() * 2 * LatitudeMaxModulus - LatitudeMaxModulus;
             var longtitude = rnd.NextDouble() * 2 * LongtitudeMaxModulus - LongtitudeMaxModulus;
 
-            var minZoom = int.Parse(Configuration["MediaOptions:MinZoom"]);
-            var maxZoom = int.Parse(Configuration["MediaOptions:MaxZoom"]);
+            var minZoom = int.Parse(_configuration["MediaOptions:MinZoom"]);
+            var maxZoom = int.Parse(_configuration["MediaOptions:MaxZoom"]);
             var zoomLevel = rnd.Next(minZoom, maxZoom);
             var request = new ImageryRequest();
 
             request.CenterPoint = new Coordinate(latitude, longtitude);
             request.ZoomLevel = zoomLevel;
-            request.BingMapsKey = Configuration["BingMapsApiKey"];
+            request.BingMapsKey = _configuration["BingMapsApiKey"];
             request.Resolution = ImageResolutionType.High;
-            request.MapHeight = int.Parse(Configuration["MediaOptions:Height"]);
-            request.MapWidth = int.Parse(Configuration["MediaOptions:Width"]);
+            request.MapHeight = int.Parse(_configuration["MediaOptions:Height"]);
+            request.MapWidth = int.Parse(_configuration["MediaOptions:Width"]);
 
             return new LocatedObject<string>(request.GetPostRequestUrl(), latitude, longtitude);
         }
 
-        private async Task<Image<Rgba32>> DownloadImage(string url)
-        {
-            using (var client = new WebClient())
-            {
+        private async Task<Image<Rgba32>> DownloadImage(string url) {
+            _logger.LogInformation($"Downloading an image from {url}");
+            using (var client = new WebClient()) {
                 var result = await client.DownloadDataTaskAsync(url);
                 return Image.Load(result);
             }
         }
 
-        private bool IsPostable(Image<Rgba32> image)
-        {
+        private bool IsPostable(Image<Rgba32> image) {
             var bwImage = image.Clone();
             bwImage.Mutate(i => i.BlackWhite());
 
             var entropy = GetEntropy(bwImage.SavePixelData());
+
+            _logger.LogInformation($"Got an image with entropy of {entropy}");
             return entropy >= Detailness;
         }
 
-        public async Task<LocatedObject<Image<Rgba32>>> GetPostableImage()
-        {
-            while (true)
-            {
+        public async Task<LocatedObject<Image<Rgba32>>> GetPostableImage() {
+            while (true) {
                 var url = GeneratePictureUrl();
                 var picture = await DownloadImage(url.GetObject());
 
@@ -77,8 +73,7 @@ namespace EarthBot.Services
             }
         }
 
-        private static unsafe double GetEntropy(byte[] data)
-        {
+        private static unsafe double GetEntropy(byte[] data) {
             int* rgi = stackalloc int[0x100], pi = rgi + 0x100;
 
             for (var i = data.Length; --i >= 0;)
@@ -92,8 +87,7 @@ namespace EarthBot.Services
             return -H / cb;
         }
 
-        private static void CropImage(Image<Rgba32> image)
-        {
+        private static void CropImage(Image<Rgba32> image) {
             image.Mutate(img => img.Crop(image.Width, image.Height - 25));
         }
     }
